7bf722145d3404b4e914373de1cc637a99bdbc0e
winimus <minimus@snu.ac.kr>
2021-04-16 21:00:59 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Nav, Navbar} from 'react-bootstrap';
+import TextComponent from './components/TextComponent'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar bg="primary" variant="dark">
+                <Navbar.Brand href="#home"> Touch Typing </Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Navbar>
+            <body>
+                <TextComponent></TextComponent>
+            </body>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/components/TextComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {}
+
+const TextComponent: React.FC<Props> = () => {
+  return (
+    <p> the quick brown fox jumps over the lazy dog </p>
+  );
+}
+
+export default TextComponent;
